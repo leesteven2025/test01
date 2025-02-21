1e728ab83a4b08a7963a2ef5a3244418c8668f00
Steven Lee <steven930318@gmail.com>
2025-02-21 17:26:15 +0800

    first commit

---
+++ demo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laragon</title>
+    </head>
+    <body>
+        <div class="container">
+        
+                  <div><a title="Getting Started" href="https://medium.com/@danchang11/webrtc-%E6%B7%BA%E8%AB%87-%E4%B8%80-%E6%A6%82%E8%BF%B0%E8%88%87%E6%9E%B6%E6%A7%8B-6050ee477b46">Getting Started</a></div>
+
+        </div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/helloworld', function () {
+    return 'Hello World';
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/users/{id}', function ($id) {
+    return 'This is a user'.$id;
+});
+
+Route::get('/test', function () {
+    $data = ['name' => 'tad', 'say' => '嗨！'];
+    return view('welcome', $data);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
