9d530a9fb419ef5ab9f32562aa516096910f665f
Steven Lee <steven930318@gmail.com>
2025-02-27 16:45:52 +0800

    add 聯絡我們頁面

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/contact.blade.php -->
+@extends('layouts.main')
+
+@section('title', '聯絡我們')
+
+@section('content')
+    <h1>聯絡我們</h1>
+    <p>如果有任何問題，請透過以下方式聯繫我們：</p>
+    <ul>
+        <li>Email: contact@example.com</li>
+        <li>電話: 0123-456-789</li>
+    </ul>
+@endsection
