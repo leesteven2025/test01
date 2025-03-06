05c936b5540c20618f634d4a44c49cb02e99f8b8
Steven Lee <steven930318@gmail.com>
2025-03-06 16:12:47 +0800

    於註冊頁面加入Address和Tel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,8 @@
                             </div>
                         </div>
 
+
+
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
@@ -59,6 +61,30 @@
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
+                        </div>
+                        
+                        <div class="row mb-3">
+                             <label for="address" class="col-md-4 text-end">{{ __('Address') }}</label>
+                                <div class="col-md-6">
+                                     <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
+                                @error('address')
+                                    <div class="invalid-feedback">
+                                    <strong>{{ $message }}</strong>
+                                    </div>
+                                 @enderror
+                                </div>
+                        </div>
+
+                         <div class="row mb-3">
+                            <label for="tel" class="col-md-4 text-end">{{ __('Tel') }}</label>
+                                <div class="col-md-6">
+                                    <input id="tel" type="text" class="form-control @error('tel') is-invalid @enderror" name="tel" value="{{ old('tel') }}" required autocomplete="tel">
+                                 @error('tel')
+                                    <div class="invalid-feedback">
+                                 <strong>{{ $message }}</strong>
+                                    </div>
+                                    @enderror
+                                 </div>
                         </div>
 
                         <div class="row mb-0">
