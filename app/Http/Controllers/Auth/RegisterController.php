7e79b4ca5f40bdc24fe6a9b1dd5d9c66231790d6
Steven Lee <steven930318@gmail.com>
2025-03-07 10:26:54 +0800

    讓address跟tel能寫入資料庫

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,6 +69,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'address'=> $data['address'],
+            'tel'=> $data['tel'],
         ]);
     }
 }
