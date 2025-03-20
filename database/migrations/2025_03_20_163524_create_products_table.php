697867630539296d92c638656e353d43938b74da
Steven Lee <steven930318@gmail.com>
2025-03-20 18:10:10 +0800

    create product's data form and details

---
+++ database/migrations/2025_03_20_163524_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');//圖片路徑
+            $table->boolean('on_sale')->default=true;
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('unitsold');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
