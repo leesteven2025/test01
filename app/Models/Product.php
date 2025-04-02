a4a0d3a97b45380b2aa157e0dfb66da64a0cdbd9
Steven Lee <steven930318@gmail.com>
2025-04-02 18:08:04 +0800

    修改商品控制器

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class Product extends Model
 {
     use HasFactory;
+    public function getImageUrlAttribute()
+    {
+        $table = 'products';
+        return Storage::disk('public')->url($this->attributes['image']);
+    }
+
 }
